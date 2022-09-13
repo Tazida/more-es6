425989f0eac05343cd60aa22f4fe4ffd1325416c
Tazida <tazidarahman5@gmail.com>
2022-09-13 22:43:13 +0600

    added more

--- chain.js
+++ chain.js
@@ -1,2 +1,29 @@
 const users = [{ id: 1, name: "abul", job: "doctor" }];
-console.log(users.name);
+// console.log(users[0].name);
+
+const data = {
+  count: 5000,
+  data: [
+    { id: 1, name: "babul", job: "leader" },
+    { id: 2, name: "dabul", job: "leader" },
+  ],
+};
+const firstJob = data.data[0].job;
+// console.log(data.data[0].job);
+
+const user = {
+  id: 5001,
+  name: "Thomas alba edison",
+  address: {
+    street: {
+      first: "35/4 kochukhet lane",
+      second: "third floor",
+      third: "right side",
+    },
+    postOffice: "cantonment",
+    city: "Dhaka",
+  },
+};
+
+const userFloor = user.address.stret?.second;
+console.log(userFloor);
